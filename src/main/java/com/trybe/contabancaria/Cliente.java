4663711a996206ade885b13407602899fc046c8f
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-06-02 12:41:22 -0300

    :sparkles: ValidaCpf e Cliente implementados

--- src/main/java/com/trybe/contabancaria/Cliente.java
+++ src/main/java/com/trybe/contabancaria/Cliente.java
@@ -9,8 +9,28 @@ public class Cliente {
   /**
    * Atributos.
    **/
+  String nome;
+  String cpf;
 
-     /**
+  /**
    * Métodos.
    **/
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    if (ValidaCpf.validarCPF(cpf)) {
+      this.cpf = cpf;
+    }
+  }
+
 }
--- src/main/java/com/trybe/contabancaria/ValidaCpf.java
+++ src/main/java/com/trybe/contabancaria/ValidaCpf.java
@@ -9,8 +9,37 @@ public class ValidaCpf {
   /**
    * Atributos.
    **/
+  private static int soma = 0;
+  private static int primeiroDV;
+  private static int segundoDV;
 
   /**
    * Métodos.
    **/
+  public static boolean validarCPF(String CPF) {
+    char[] cpf = CPF.toCharArray();
+    for (int i = 2; i < 11; i++) {
+      soma += cpf[-i + 1] * i;
+    }
+    if (11 - (soma % 11) < 10) {
+      primeiroDV = 0;
+    } else {
+      primeiroDV = 11 - (soma % 11);
+    }
+    soma = 0;
+    for (int i = 2; i <= 11; i++) {
+      soma += cpf[-i] * i;
+    }
+    if (11 - (soma % 11) < 10) {
+      segundoDV = 0;
+    } else {
+      segundoDV = 11 - (soma % 11);
+    }
+
+    if (primeiroDV == cpf[-2] && segundoDV == cpf[-1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
